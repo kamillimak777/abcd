511756971d664d3049b7d18fe865897fd1f81b76
Mateusz Kulesza <ev45ive@gmail.com>
2022-11-21 16:11:26 +0100

    Exercise: Fix 'edit' 'cancel' buttons to switch modes

--- apps/demo/src/app/playlists/containers/playlists-view/playlists-view.container.ts
+++ apps/demo/src/app/playlists/containers/playlists-view/playlists-view.container.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-playlists-view',
   templateUrl: './playlists-view.container.html',
-  styleUrls: ['./playlists-view.container.css']
+  styleUrls: ['./playlists-view.container.css'],
 })
 export class PlaylistsViewContainer implements OnInit {
+  mode: 'details' | 'editor' = 'details';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  showEditor() {
+    this.mode = 'editor'
+  }
+  
+  showDetails() {
+    this.mode = 'details'
   }
 
+  ngOnInit(): void {}
 }
