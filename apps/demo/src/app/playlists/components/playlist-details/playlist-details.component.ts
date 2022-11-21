0f19151a328618a494e4521d7a7d99ab51ca8feb
Mateusz Kulesza <ev45ive@gmail.com>
2022-11-21 11:59:45 +0100

    [style], [class.*], [ngStyle], [ngClass], View Encapsulation, :host  ::ng-deep

--- apps/demo/src/app/playlists/components/playlist-details/playlist-details.component.ts
+++ apps/demo/src/app/playlists/components/playlist-details/playlist-details.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-playlist-details',
   templateUrl: './playlist-details.component.html',
-  styleUrls: ['./playlist-details.component.css'],
+  styleUrls: ['./playlist-details.component.scss'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class PlaylistDetailsComponent implements OnInit {
   playlist = {
