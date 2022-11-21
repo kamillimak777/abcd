dd58cfedb241efc54e7f3e376b74cbf148aeaf6f
Mateusz Kulesza <ev45ive@gmail.com>
2022-11-21 12:18:46 +0100

    Exercise: Bind data to form field values

--- apps/demo/src/app/playlists/components/playlist-editor/playlist-editor.component.ts
+++ apps/demo/src/app/playlists/components/playlist-editor/playlist-editor.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-playlist-editor',
   templateUrl: './playlist-editor.component.html',
-  styleUrls: ['./playlist-editor.component.css']
+  styleUrls: ['./playlist-editor.component.css'],
 })
 export class PlaylistEditorComponent implements OnInit {
+  playlist = {
+    id: '123',
+    name: 'Playlist 123',
+    public: true,
+    description: 'Awesome Playlist',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
