4021882b4dbd59528c99efd6662ba9225c03208d
Mateusz Kulesza <ev45ive@gmail.com>
2022-11-21 14:02:13 +0100

    ZoneJS - ApplicationRef, NgZone, ChangeDetectorRef, zone.runOutsideAngular, detectChanges

--- apps/demo/src/app/shared/components/clock/clock.component.ts
+++ apps/demo/src/app/shared/components/clock/clock.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ApplicationRef,
+  ChangeDetectorRef,
+  Component,
+  NgZone,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
-  styleUrls: ['./clock.component.css']
+  styleUrls: ['./clock.component.css'],
 })
 export class ClockComponent implements OnInit {
+  time = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private app: ApplicationRef,
+    private zone: NgZone, // zone.js
+    private cdr: ChangeDetectorRef
+  ) {
+    // this.cdr.detach();
   }
 
+  updateTime() {
+    this.time = new Date().toLocaleTimeString();
+  }
+
+  ngOnInit(): void {
+    // this.cdr.detectChanges();
+
+    this.zone.runOutsideAngular(() => {
+      setInterval(() => {
+        this.updateTime();
+        this.cdr.detectChanges();
+      }, 1_000);
+    });
+  }
 }
