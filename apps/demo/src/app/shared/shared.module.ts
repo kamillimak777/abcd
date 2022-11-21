b122a000ac034556a477627597f9b7ffa44d05c5
Mateusz Kulesza <ev45ive@gmail.com>
2022-11-21 11:28:50 +0100

    Reusable Layout + Content Projection <ng-content>

--- apps/demo/src/app/shared/shared.module.ts
+++ apps/demo/src/app/shared/shared.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClockComponent } from './components/clock/clock.component';
 
-import {MatListModule} from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
+import { NavigationComponent } from './layouts/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [ClockComponent],
-  imports: [CommonModule, MatListModule],
-  exports: [ClockComponent, MatListModule],
+  declarations: [ClockComponent, NavigationComponent],
+  imports: [
+    CommonModule,
+    MatListModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+  ],
+  exports: [ClockComponent, MatListModule, NavigationComponent],
 })
 export class SharedModule {}
