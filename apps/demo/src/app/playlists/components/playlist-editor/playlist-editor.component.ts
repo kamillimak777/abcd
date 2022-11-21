a7c9e8fffffbc0182671196c78faa5899321c92c
Mateusz Kulesza <ev45ive@gmail.com>
2022-11-21 15:25:10 +0100

    @ViewChild(NgForm), ngAfterViewInit, setValue, resetForm

--- apps/demo/src/app/playlists/components/playlist-editor/playlist-editor.component.ts
+++ apps/demo/src/app/playlists/components/playlist-editor/playlist-editor.component.ts
@@ -1,10 +1,17 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-playlist-editor',
   templateUrl: './playlist-editor.component.html',
   styleUrls: ['./playlist-editor.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlaylistEditorComponent implements OnInit {
   playlist = {
@@ -14,9 +21,36 @@ export class PlaylistEditorComponent implements OnInit {
     description: 'Awesome Playlist',
   };
 
+  // @ViewChild('ref',{read: ElementRef})
+  // @ViewChild('ref', { read: NgModel })
+  // modelRef?: NgModel;
+  // @ViewChild(NgForm)
+  @ViewChild('formRef', { read: NgForm })
+  formRef?: NgForm;
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+  }
+
+  submit() {
+    if (this.formRef?.invalid) return;
+    console.log(this.formRef?.value);
+
+    // this.formRef?.setValue({
+    //   name: '',
+    //   public: false,
+    //   description: '',
+    // });
+    
+    this.formRef?.resetForm({
+      name: '',
+      public: false,
+      description: '',
+    })
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
-
-
 }
