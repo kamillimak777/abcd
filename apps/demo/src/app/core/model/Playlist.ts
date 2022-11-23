795164c3f29f22872619f2d1b2cab9b8c044d1b2
Mateusz Kulesza <mateusz@altkom.pl>
2022-11-23 10:09:00 +0100

    Type Narrowing - (x != null) // if (typeof x == primitive) // if (x instanceof Constructor) // if ('key' in x )

--- apps/demo/src/app/core/model/Playlist.ts
+++ apps/demo/src/app/core/model/Playlist.ts
@@ -1,9 +1,52 @@
+interface Track { id: string, name: string; duration_ms: number }
+interface Episode { id: string, name: string;   }
+
 export interface Playlist {
   id: string;
   name: string;
   public: boolean;
   description: string;
+  tracks?: Track[]
 }
+
+
+// if (x != null)
+// if (typeof x == primitive)
+// if (x instanceof Constructor)
+// if ('key' in x )
+
+
+function getInfo(res: Playlist | Track ) {
+  if ('public' in res) {
+    return `${res.name} has ${res.tracks?.length ?? 'no'} tracks`
+  }
+  if ('duration_ms' in res) {
+    return `${res.name} has ${res.duration_ms}ms length`
+  }
+  exhaustivenessCheck(res)
+}
+
+
+/// ==================
+// p is unknown
+const p = {} as Playlist
+///
+
+if (p.tracks) {
+  const len0 = p.tracks.length
+};
+
+const len1 = p.tracks ? p.tracks.length : 0
+const len2 = p.tracks?.length
+const len3 = p.tracks?.length || 0
+const len4 = p.tracks?.length ?? 0
+
+// Hacks - Casting
+const len5 = p.tracks?.length as any
+const len6 = p.tracks?.length as number
+const len7 = p.tracks?.length!
+
+/// ==================
 
 type AllowedIdTypes = number | string // | object;
 
@@ -31,3 +74,4 @@ function exhaustivenessCheck(_shouldNotHappen: never): never {
 }
 
 // const res = parseId({} as 123)
+
