e332b0097cfae8e3a30499b95c37e600dad2376b
Mateusz Kulesza <mateusz@altkom.pl>
2022-11-23 09:50:38 +0100

    Exhaustiveness Check, const _shouldNotHappen: never = id

--- apps/demo/src/app/core/model/Playlist.ts
+++ apps/demo/src/app/core/model/Playlist.ts
@@ -4,3 +4,26 @@ export interface Playlist {
   public: boolean;
   description: string;
 }
+
+type AllowedIdTypes = number | string // | object;
+
+// Nowy Ekran
+
+function parseId(id: AllowedIdTypes) {
+  // return id.toString() 
+
+  // Type Narrowing
+  if (typeof id === 'number') {
+    return id.toFixed(2)
+  }
+
+  if (typeof id === 'string') {
+    return id.toUpperCase()
+  }
+
+  // Exhaustiveness Check
+  const _shouldNotHappen: never = id
+  throw new Error('Unexpected id type!')
+}
+
+const res = parseId({} as 123)
